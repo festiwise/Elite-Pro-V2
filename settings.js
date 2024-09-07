31cffbeb63943483595b89bebd8d94bf20949592
festiwise <wasiedu1234@gmail.com>
2024-09-07 16:41:09 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,8 +3,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "2348109263390"
-global.ownername = "Elite-Pro-V2"
+global.ownernomer = "14168483856"
+global.ownername = "yngsteez"
 global.ytname = "YT: ElitePro"
 global.socialm = "GitHub: elite-md"
 global.location = "Nigeria, Port Harcourt"
